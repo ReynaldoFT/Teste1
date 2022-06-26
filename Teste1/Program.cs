756eba786364bfafa15ed95845fd1e0483fd5238
Naja Info <Naja Info@NALDINHO>
2022-06-26 14:36:07 -0300

    Adicionar arquivos de projeto.

---
+++ Teste1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste1
+{
+     internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b = 0, c;
+
+            Console.WriteLine("Digite um numero");
+            a = Convert.ToDouble(Console.ReadLine());
+
+            while (b < 10)
+            {
+                b = b + 1;
+                c = a * b;
+                Console.WriteLine(a + "*" + b + "=" + c);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
